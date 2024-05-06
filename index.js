f5b5229cfa5ad9df65c04ecb905e1dd71b2b2945
JosephAJJames <130866326+JosephAJJames@users.noreply.github.com>
2024-05-06 16:21:27 +0000

    Added functions

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+const sumOfElems = (arr) => {
+  total = 0;
+  arr.forEach((elem) => {
+    total += elem;
+  })
+  console.log(total);
+}
 
+sumOfElems([1, 2, 3, 4]);
+sumOfElems([5, 4, 9, 12]);
 
+const findMax = (arr) => {
+  max = 0;
+  arr.forEach((elem, index) => {
+    if (index === 0) {
+      max = elem;
+    }
+    
+    if (elem > max) {
+      max = elem;
+    }
+  })
+  console.log(max);
+}
+
+findMax([8, 2, 3, 5]);
+findMax([4, 2, 7, 5]);
